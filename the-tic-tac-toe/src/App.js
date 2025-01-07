d9b22d66c0340bd2fbed7180d239ab34999cb144
Darren Gonsalves <dgx@DGX-MBP.local>
2025-01-07 21:30:24 +0530

    Game Score Header, Message and Game Board

--- the-tic-tac-toe/src/App.js
+++ the-tic-tac-toe/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import { useState } from "react";
+import PlayerScore from "./components/PlayerScore";
+import GameTitleHeader from "./components/GameTitleHeader";
+import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [playerOneScore, setPlayerOneScore] = useState(0);
+  const [playerTwoScore, setPlayerTwoScore] = useState(0);
+  const [gameMessage, setGameMessage] = useState("Player 1 turn");
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="border border-white">Tic Tac Toe</p>
-      </header>
+    <div className="App bg-slate-950 min-h-full text-white">
+      <GameTitleHeader />
+      <PlayerScore
+        playerOneScore={playerOneScore}
+        playerTwoScore={playerTwoScore}
+        gameMessage={gameMessage}
+      />
+      <GameBoard />
     </div>
   );
 }
---
+++ the-tic-tac-toe/src/components/GameTitleHeader.js
@@ -0,0 +1,11 @@
+function GameTitleHeader() {
+  return (
+    <h1 className="bg-slate-950 text-white text-5xl py-3 px-3 font-serif">
+      <div className="px-3 py-2 rounded-md text-center tracking-widest">
+        THE TIC-TAC-TOE
+      </div>
+    </h1>
+  );
+}
+
+export default GameTitleHeader;
